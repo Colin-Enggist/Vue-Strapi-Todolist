6f977d0df189143ccc5a8e95483812a46ee8c26f
Colin Enggist <99475091+Kokossnuss@users.noreply.github.com>
2022-10-21 10:35:47 +0200

    working finished Todo-Card

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,54 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+<div class="wrapper">
+  <TodoCard v-for="items in items" :key="items.key"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TodoCard from "./components/TodoCard"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoCard
+  },
+  methods:{
+    getPost(){
+      fetch('http://localhost:1337/api/todos',{
+        method: 'GET',
+      })
+      .then((response)=>response.json())
+      .then(data=>{return data.data})
+      .catch((error)=>console.log(error))
+    }
+  },
 }
 </script>
 
 <style>
+html{
+  background-color: lightgray;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+.wrapper{
+  width: 80%;
+  height: 92vh;
+  overflow: scroll;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: white;
+  overflow-y: hidden; /* Hide vertical scrollbar */
+  overflow-x: hidden; /* Hide horizontal scrollbar */
+  justify-content: space-around;
 }
 </style>
---
+++ app/src/components/TodoCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="todolist">
+        <h2>{{date}}</h2>
+        <div class="content">
+            <h1>{{todoName}}</h1>
+            <p>{{todoDescribtion}}</p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+export default{
+    name: "TodoCard",
+    props:{
+        date: String,
+        todoName: String,
+        todoDescribtion: String,
+    },
+}
+</script>
+
+<style>
+.todolist{
+    width: 300px;
+    height: 400px;
+    border: 5px solid black;
+    border-radius: 30px;
+    position: relative;
+    margin-top: 18px;
+}
+h2{
+    position: absolute;
+    top: 10px;
+    left: 20px;
+}
+.content{
+    width: 100%;
+    height: 82%;
+    position: absolute;
+    bottom: 0;
+    border-bottom-right-radius: 30px;
+    border-bottom-left-radius: 30px;
+    border-top: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    overflow: scroll;
+    overflow-y: hidden; /* Hide vertical scrollbar */
+  overflow-x: hidden; /* Hide horizontal scrollbar */
+}
+
+h1{
+    margin-left: 20px;
+}
+
+p{
+    margin-left: 25px;
+    font-size: 20px;
+    margin-top: 0;
+}
+</style>
